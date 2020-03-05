a9eff1d2177f7898221bc79ff586acb133826aae
Sharang Dashputre <sharang.d@gmail.com>
2020-03-05 11:08:38 +0530

    Change PP button UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,43 +7,13 @@
 
 $(function() {
   if($('#paypal-button-container').length > 0) {
-    // paypal.Button.render({
-    //   env: 'sandbox', // sandbox | production
-    //   // Show the buyer a 'Pay Now' button in the checkout flow
-    //   commit: true,
-    //   // payment() is called when the button is clicked
-    //   payment: function() {
-    //       // Set up a url on your server to create the payment
-    //       var CREATE_URL = '/orders/';
-    //
-    //       // Make a call to your server to set up the payment
-    //       return paypal.request.post(CREATE_URL)
-    //           .then(function(res) {
-    //             return res.paymentID;
-    //           });
-    //   },
-    //
-    //   // onAuthorize() is called when the buyer approves the payment
-    //   onAuthorize: function(data, actions) {
-    //       // Set up a url on your server to execute the payment
-    //       var EXECUTE_URL = '/orders/pay';
-    //       // // Set up the data you need to pass to your server
-    //       // var data = {
-    //       //     paymentID: data.paymentID,
-    //       //     payerID: data.payerID
-    //       // };
-    //
-    //       // // Make a call to your server to execute the payment
-    //       return paypal.request.post(EXECUTE_URL, data)
-    //           .then(function (res) {
-    //               window.alert('Payment Complete!');
-    //           });
-    //       // return actions.redirect();
-    //
-    //   }
-    // }, '#paypal-button-container');
-
     paypal.Buttons({
+      style: {
+        layout: 'horizontal',
+        color: 'silver',
+        shape: 'pill',
+        tagline: false
+      },
       createOrder: function(data, actions) {
         return fetch('/orders', {
           method: 'POST'
